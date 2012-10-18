2d44c7f20b6afdf8c68777f031d042bd4dee1ef3
Kyle Phillips <kyle@haptic-data.com>
2012-10-17 23:20:26 -0500

    added subdiv example

--- www/javascripts/config.js
+++ www/javascripts/config.js
@@ -1,11 +1,12 @@
-requirejs = {
+var requirejs = {
   baseUrl: 'javascripts/vendor',
   shim: {
     underscore: { exports: '_' },
-    backbone: { deps: ['jquery', 'underscore'], exports: 'Backbone' }
+    backbone: { deps: ['jquery', 'underscore'], exports: 'Backbone' },
+    three: { exports: 'THREE' }
   },
   paths: {
     'toxiclibsjs': '../toxiclibsjs',
-    'toxi': 'https://raw.github.com/hapticdata/toxiclibsjs/develop/lib/toxi'
+    'toxi': '../../../../working_dir/lib/toxi' //https://raw.github.com/hapticdata/toxiclibsjs/feature_wemesh/lib/toxi'
   }
 };
