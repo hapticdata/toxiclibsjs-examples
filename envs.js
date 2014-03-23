9b4c67f04a4a9c4f9f8e8e599936ccd94889d394
Kyle Phillips <kyle@haptic-data.com>
2014-03-23 15:56:15 -0700

    updated some copy in footer & index
    retabbed envs.js and flattened organization, retabbed tcolorPjs.pde

--- envs.js
+++ envs.js
@@ -1,51 +1,56 @@
 /*global __dirname,process*/
 var fs = require('fs'),
-	_ = require('underscore');
+    _ = require('underscore'),
+    defaults,
+    creds;
+
+
+//all configurations
+defaults = {
+    s3: {
+        key: process.env.S3_KEY,
+        secret: process.env.S3_SECRET,
+        bucket: 'toxiclibsjs',
+        access: 'public-read'
+    },
+    doccoPath: 'generated/docs/',
+    stylesheetsPath: 'generated/stylesheets/',
+    baseUrl: 'src/',
+    toxiclibsjsDir: 'node_modules/toxiclibsjs/',
+    layout: 'src/views/layout.jade',
+    examples: 'src/javascripts/examples/',
+    //the root location of the site
+    rootUrl: '/',
+    pretty: true,
+    port: 3004,
+    compress: false
+};
+
+creds = {
+    dev: {
+        //the root location for the static assets over http
+        staticUrl: '/',
+        //the hard-disk location of static files to serve during development
+        staticDir: 'src/'
+    },
+    staging: {
+        staticUrl: '/',
+        staticDir: 'dist/'
+    },
+    production: {
+        staticUrl: 'http://s3.amazonaws.com/toxiclibsjs/',
+        //the root location of the site
+        rootUrl: '/toxiclibsjs/',
+        //the hard-disk location of files to upload to s3
+        staticDir: 'dist/',
+        compress: true
+    }
+};
 
 /**
  * Various credentials and other protected information
  */
 exports = module.exports = function( env, config ){
-	env = env || process.env.NODE_ENV;
-	var creds = {
-		defaults: {
-			s3: {
-				key: process.env.S3_KEY,
-				secret: process.env.S3_SECRET,
-                bucket: 'toxiclibsjs',
-                access: 'public-read'
-            },
-            doccoPath: 'generated/docs/',
-            stylesheetsPath: 'generated/stylesheets/',
-			baseUrl: 'src/',
-            toxiclibsjsDir: 'node_modules/toxiclibsjs/',
-			layout: 'src/views/layout.jade',
-			examples: 'src/javascripts/examples/',
-			//the root location of the site
-			rootUrl: '/',
-			pretty: true,
-            port: 3004,
-			compress: false
-		},
-		dev: {
-			//the root location for the static assets over http
-            staticUrl: '/',
-			//the hard-disk location of static files to serve during development
-            staticDir: 'src/'
-		},
-		staging: {
-			staticUrl: '/',
-			staticDir: 'dist/'
-		},
-		production: {
-            staticUrl: 'http://s3.amazonaws.com/toxiclibsjs/',
-			//the root location of the site
-			rootUrl: '/toxiclibsjs/',
-			//the hard-disk location of files to upload to s3
-            staticDir: 'dist/',
-			compress: true
-		}
-	};
-
-	return _.defaults( _.defaults(creds[env], config || {}), creds.defaults );
+    env = env || process.env.NODE_ENV;
+    return _.defaults( _.defaults(creds[env], config || {}), defaults );
 };
